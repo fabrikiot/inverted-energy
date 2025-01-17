cc785d44164a1c172fd8bf4027becf5738bdb206
koushikgg <iamkoushikg@gmail.com>
2025-01-17 15:17:15 +0530

    fix the scroll

--- inverted-energy/src/features/nav-bar/navbar.tsx
+++ inverted-energy/src/features/nav-bar/navbar.tsx
@@ -2,10 +2,10 @@ import myImage from "../../assets/Inverted-Logo.png";
 
 export const Navbar = () => {
   return (
-    <div className="w-full h-20 flex flex-row justify-between p-2 border-b-2 border-[#41E1F2] items-center">
+    <div className="w-full h-full flex flex-row justify-between p-2 border-b-2 border-[#41E1F2] items-center">
       <div className="text-[#41E1F2] mx-20 font-bold  text-3xl">FLUX</div>
       <div className="mx-20 ">
-        <img src={myImage} alt="" className="h-[62px] w-[150px]" />
+        <img src={myImage} alt="inverted" className="h-[54px] w-[150px]" />
       </div>
     </div>
   );
